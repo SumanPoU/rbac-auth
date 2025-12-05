a8f82e76ab14c09fee5ef41543beb64dfd3d4e37
SumanPoU <acharyas186@gmail.com>
2025-12-05 23:12:12 +0545

    ::added:: cookiee constant

--- src/components/session-provider.tsx
+++ src/components/session-provider.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import CookieConsent from "@/components/cookie-consent";
 
 export default function AuthProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <CookieConsent
+        variant="default"
+        onAcceptCallback={() => console.log("Accepted")}
+        onDeclineCallback={() => console.log("Declined")}
+      />
+      {children}
+    </SessionProvider>
+  );
 }
